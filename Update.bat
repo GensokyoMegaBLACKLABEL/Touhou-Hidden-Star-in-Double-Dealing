65f4ecc6df6f2a3b9093ab6c11d86d0f880a0443
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-03 17:44:28 +0700

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,2 +1,2 @@
-C:\Users\win10\AppData\Local\Programs\Python\Python39\python.exe D:\thcrap\repos\Touhou-Hidden-Star-in-Double-Dealing
+C:\Users\win10\AppData\Local\Programs\Python\Python39\python.exe D:\thcrap\repos\Touhou-Hidden-Star-in-Double-Dealing\repo_update.py
 pause
