e1c777add7480676c7a32c11e6cc45e6d71fbe4c
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-04 14:24:01 +0700

    Add files via upload

---
+++ Touhou Hidden Star in Double Dealing/repo.js
@@ -0,0 +1,9 @@
+{
+	"contact": "phandin1951@gmail.com",
+	"id": "Touhou-Hidden-Star-in-Double-Dealing",
+	"patches": {},
+	"servers": [
+		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/Touhou-Hidden-Star-in-Double-Dealing/master"
+	],
+	"title": "Auto's Patch Repo"
+}
