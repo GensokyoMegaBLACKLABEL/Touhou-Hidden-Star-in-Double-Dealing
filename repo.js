1edffe6d6f87a62216f5ef6829682a528a8bb807
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-04 14:21:48 +0700

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,7 +1,7 @@
 {
 	"contact": "phandin1951@gmail.com",
 	"id": "AutoThaiLand",
-	"patches": {"LoDDK": "(TH16) A boss rush patch in which you fight bosses from DDC and LoLK at once XD.",
+	"patches": {"HSiDDC": "(TH16) A boss rush patch in which you fight bosses from DDC and Hidden Star in Four Season at once XD.",
 		},
 	"servers": [
 		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/Touhou-Hidden-Star-in-Double-Dealing/master"
