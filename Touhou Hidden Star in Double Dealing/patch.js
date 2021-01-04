2886e1b5fbdde587fc4a8dd58af14e3262037d23
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-04 13:19:48 +0700

    Create patch.js

---
+++ Touhou Hidden Star in Double Dealing/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "HSiDDC",
+	"servers": [
+		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/master/Touhou Hidden Star in Double Dealing"
+	],
+	"title": "(TH16) A boss rush patch in which you fight bosses from DDC and HSiFS at once XD."
+}
